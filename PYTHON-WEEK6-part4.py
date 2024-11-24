cb9843befdd9e7783d5a067cf7052fa872f28e4b
davershayan12 <davershayan13@gmail.com>
2024-11-24 22:42:46 +0500

    .

---
+++ PYTHON-WEEK6-part4.py
@@ -0,0 +1,10 @@
+i = 10 
+if (i <  15):
+    #First if statementif (i <  15):
+    print("i is smaller than 15")
+    if(i < 12):
+        # Nested -  if statement
+        # Will only be executed if statement above# it is true if (i < 12):
+        print("i is smaller than 12 too")
+else:
+    print ("i is greater than 15")
