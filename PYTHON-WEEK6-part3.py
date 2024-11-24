2723e4d3497cae6334cd4205ec2dbfcc960f3816
davershayan12 <davershayan13@gmail.com>
2024-11-24 22:35:10 +0500

    .

--- PYTHON-WEEK6-part3.py
+++ PYTHON-WEEK6-part3.py
@@ -2,7 +2,6 @@ balance = int(input("enter yourbalance1:"))
 while True:
     if balance <=9000:
         balance = balance+999.99
+        print("Balance is", balance)
         continue
-
-    print("Balance is", balance)
     break
