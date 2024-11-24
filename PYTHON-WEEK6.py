464e52e55ec09d3ad650983ef26c33cea9261fbe
davershayan12 <davershayan13@gmail.com>
2024-11-24 19:03:01 +0500

    .

--- PYTHON-WEEK6.py
+++ PYTHON-WEEK6.py
@@ -3,6 +3,6 @@ prompt+="\n(Enter 'quit' when you are finished.)"
 while True:
     city =input(prompt)
     if city =="quit":
-        break 
-else:
-    print("I'd love to go to " , city.title() , "!")
+       break 
+    else:
+       print("I'd love to go to " , city.title() , "!")
