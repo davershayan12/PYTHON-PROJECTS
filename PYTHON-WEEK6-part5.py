94a1cf91972633f2b1dfe837b6ae893a9352de72
davershayan12 <davershayan13@gmail.com>
2024-11-26 00:14:26 +0500

    .

--- PYTHON-WEEK6-part5.py
+++ PYTHON-WEEK6-part5.py
@@ -1,8 +1,7 @@
 #1.	Write a program to add first seven terms twice of the following series:
 #1/1!+2/2!+....
 i=1
-fac=1
+
 while i <= 7:
-    fac*=i
-    print(f'{i}/{fac}!')
+    print(f'{i}/{i}!')
     i+=1
---
+++ PYTHON-WEEK6-part6.py
@@ -0,0 +1,10 @@
+'''2.	Write a program to print all prime numbers from 900 to 1000. 
+[Hint: Use nested loops, break and continue]
+'''
+i=900
+num=2
+while i<=1000:
+    if(i==num):
+        continue
+    while 
+    i+=1
