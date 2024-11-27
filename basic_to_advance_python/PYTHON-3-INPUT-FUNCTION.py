4ddb308575bcdb90eadc999a3e5ceb57f55169cd
davershayan12 <davershayan13@gmail.com>
2024-11-27 19:55:45 +0500

    .

---
+++ basic_to_advance_python/PYTHON-3-INPUT-FUNCTION.py
@@ -0,0 +1,6 @@
+name=input('Enter your Name: ')#by defult,input function take input as string. 
+age=input('Enter your age: ')#To obtain an integer value from the user.
+realAge=int(input('Enter your age: '))
+print('Your name is: ',name)
+print(type(age))#string
+print(type(realAge))#integer
