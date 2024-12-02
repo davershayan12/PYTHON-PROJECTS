baa62031140ba70d0fcd01c353c75c6c89adc24e
davershayan12 <davershayan13@gmail.com>
2024-12-02 13:19:02 +0500

    .

---
+++ basic_to_advance_python/PYTHON-4-ARiTHEMATICS-OPERATOR.py
@@ -0,0 +1,14 @@
+#Arithematics operator are use to do Arithematics operation.
+a=1
+b=2
+#for addition:
+print(a+b)
+#for subtraction:
+print(a-b) 
+#for multiplication:
+print(a*b)
+#for division:
+print(a/b)
+#for floor division:
+print(a//b)
+#floor division is use to divide two number but it will always give answer in integer
