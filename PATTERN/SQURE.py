15e07c98b4c9c5f76f4233201789cd386c6ef89c
davershayan12 <davershayan13@gmail.com>
2024-12-04 02:28:36 +0500

    .

---
+++ PATTERN/SQURE.py
@@ -0,0 +1,15 @@
+i=1
+while i<=5:
+    j=1
+    if i==1:
+        while j<=9:
+            print('*',end=' ')
+            j+=1
+        print()
+    print('*\t\t*')
+    if i==5:
+        while j<=9:
+            print('*',end=' ')
+            j+=1
+    
+    i+=1
