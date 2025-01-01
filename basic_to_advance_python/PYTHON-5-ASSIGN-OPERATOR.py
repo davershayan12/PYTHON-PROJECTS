718f1776f59cc5e75019b9a922d88ac1758c92cd
davershayan12 <davershayan13@gmail.com>
2025-01-01 22:15:08 +0500

    .

--- basic_to_advance_python/PYTHON-5-ASSIGN-OPERATOR.py
+++ basic_to_advance_python/PYTHON-5-ASSIGN-OPERATOR.py
@@ -1,30 +1,30 @@
 a=3
-# ↑=This is assgnment operator.
+# ↑->This is assgnment operator.
 print(a)
 a+=2
-# ↑='+='operator is use to add the value.
+# ↑->'+='operator is use to add the value.
 print(a)
 a=3
 a-=1
-# ↑='-='operator is use to subtract the value.
+# ↑->'-='operator is use to subtract the value.
 print(a)
 a=3
 a*=2
-# ↑='*='operator is use to multiply the value.
+# ↑->'*='operator is use to multiply the value.
 print(a)
 a=3
 a/=2
-# ↑='/='operator is use to divide the value.
+# ↑->'/='operator is use to divide the value.
 print(a)
 a=3
 a//=2
-# ↑='//='operator is use to floor divide the value.
+# ↑->'//='operator is use to floor divide the value.
 print(a)
 a=3
 a%=2
-# ↑='%='operator is use to get the remainder.
+# ↑->'%='operator is use to get the remainder.
 print(a)
 a=3
 a**=2
-# ↑='**='operator is use to get the power of the value.
+# ↑->'**='operator is use to get the power of the value.
 print(a)
