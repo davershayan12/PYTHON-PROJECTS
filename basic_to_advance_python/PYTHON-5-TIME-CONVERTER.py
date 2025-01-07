e4e653cacc72f4a96dc9e27458e62a547895db16
davershayan12 <davershayan13@gmail.com>
2025-01-08 00:30:28 +0500

    .

--- basic_to_advance_python/PYTHON-5-TIME-CONVERTER.py
+++ basic_to_advance_python/PYTHON-5-TIME-CONVERTER.py
@@ -1,4 +1,8 @@
 #there are two method to convert time in seconds to days, hours, minutes and seconds.
-#Method 1:
+#Method 1(main):
 time=int(input('enter your time in seconds:'))
-day
+days=time//86400
+hours=(time%86400)//3600
+minutes=(time%3600)//60
+seconds=time%60
+print(f"days: {days},hours: {hours},minutes: {minutes},seconds: {seconds}")
