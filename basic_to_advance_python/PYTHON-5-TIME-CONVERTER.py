672db398cd1430a43eee79d3a1ba514ab6c5c959
davershayan12 <davershayan13@gmail.com>
2025-01-08 00:40:45 +0500

    .

--- basic_to_advance_python/PYTHON-5-TIME-CONVERTER.py
+++ basic_to_advance_python/PYTHON-5-TIME-CONVERTER.py
@@ -9,9 +9,9 @@ print(f"days: {days},hours: {hours},minutes: {minutes},seconds: {seconds}")#answ
 
 
 #method 2(main):
-time=int(input('enter your time in seconds:'))
-days=time//86400
-hours=(time%86400)//3600
-minutes=(time%3600)//60
-seconds=time%60
-print(f"days: {days},hours: {hours},minutes: {minutes},seconds: {seconds}")
+time=int(input('enter your time in seconds:'))#take time in seconds
+days=time//86400 # Calculate the number of days by dividing the time by the total seconds in a day (86400)
+hours=(time%86400)//3600 # Calculate the number of hours by finding the remainder of time after days and dividing by the total seconds in an hour (3600)
+minutes=(time%3600)//60 # Calculate the number of minutes by finding the remainder of time after extracting hours and dividing by 60
+seconds=time%60 # Calculate the remaining seconds after extracting days, hours, and minutes
+print(f"days: {days},hours: {hours},minutes: {minutes},seconds: {seconds}")#answer
