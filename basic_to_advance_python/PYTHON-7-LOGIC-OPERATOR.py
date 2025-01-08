418ed40b04381934ab8e7f7b12d65eff227afe9f
davershayan12 <davershayan13@gmail.com>
2025-01-08 20:55:51 +0500

    .

--- basic_to_advance_python/PYTHON-7-LOGIC-OPERATOR.py
+++ basic_to_advance_python/PYTHON-7-LOGIC-OPERATOR.py
@@ -1,11 +1,18 @@
 #Logical operators are tools used in programming and math to combine or compare conditions. They help decide if something is true or false.
 '''and operator''' 
+#All conditions must be true.
 print('10>5 and 4<3: ',10>5 and 4<3)
 #      True  and Fasle =>False
 print('10>5 and 4>3: ',10>5 and 4>3)  
 #      True  and True =>True
+
 '''or operator'''
-print('105  or  4<3: ',10>5 or 4<3)
+#At least one condition must be true.
+print('10>5  or  4<3: ',10>5 or 4<3)
 #      True or Flase=>True
 print('10<5  or  4<3: ',10>5 or 4<3)
 #      False or Fasle =>True
+'''or operator'''
+#Flips the truth or false
+print('10<5: ',not(10<5))
+
