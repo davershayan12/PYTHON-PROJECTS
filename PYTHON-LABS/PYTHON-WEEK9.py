c62d3e44634a36a9d864c22783ff33aa6cf8555d
davershayan12 <davershayan13@gmail.com>
2025-01-23 09:11:16 +0500

    .

---
+++ PYTHON-LABS/PYTHON-WEEK9.py
@@ -0,0 +1,34 @@
+# # def guess_the_capitals():
+# #     correct = 0
+# #     incorrect = 0
+# #     capitals = {"Canada": "Ottawa",
+# #     "Germany": "Berlin",
+# #     "France": "Paris",
+# #     "United Kingdom": "London",
+# #     "Italy": "Rome",
+# #     "Spain": "Madrid",
+# #        }
+# #     for key , value in capitals.items():
+# #        userguess=input(f"enter your guess of {key}: ") 
+# #        if userguess== value:
+# #            correct+=1
+# #            print('coreect answer!')
+# #        else:
+# #            incorrect+=1
+# #            print('incoreect answer!')
+# #     print(f'coreect:{correct}    incoreect:{incorrect}')
+# # guess_the_capitals()
+
+
+
+# favouritePlaces={
+#     "Alice": ["Paris", "New York", "Tokyo"],
+#     "Bob": ["London", "Sydney", "Rome"],
+#     "Charlie": ["Berlin", "Amsterdam", "Barcelona"]
+# }
+
+# for key,value in favourite_places.items():
+#     print(f"{key} favourite place is:")
+#     for i in range(3):
+       
+#        print(f"{value[i]}")
