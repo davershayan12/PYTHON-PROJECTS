4fecf200f2af246b98b7dac7b838633014f379ee
davershayan12 <davershayan13@gmail.com>
2025-05-16 13:16:08 +0500

    .

--- basic_to_advance_python/PYTHON--.py
+++ basic_to_advance_python/PYTHON--.py
@@ -1,55 +1,65 @@
 import random
 
 def player() -> list:
-    roles = {"warrior": [110, 20, "Dimension Strike", "Iron Slash", "Defence"],"archer": [100, 30, "Fire Arrow", "Multiple Arrow", "Defence"],"dagger": [90, 40, "One Point Slash", "Back Slash", "Defence"]}
+    roles = {"warrior": [110, 20, "Dimension Strike", "Iron Slash", "Defence"],
+             "archer": [100, 30, "Fire Arrow", "Multiple Arrow", "Defence"],
+             "dagger": [90, 40, "One Point Slash", "Back Slash", "Defence"]}
     print("Enter your role")
     for role, stats in roles.items():
         print(f"{role}:\n HP: {stats[0]}\n Attack power: {stats[1]}\n 1.Attack: {stats[2]}\n 2.Attack: {stats[3]}\n 3.{stats[4]}")
     while True:
         role = input().lower()
         if role in roles:
-            player = roles[role][:]
-            return player
+            playerRole = roles[role]
+            return playerRole
         else:
-            print("invaild choice! try again")
+            print("Invalid choice! Try again")
 
 def battle_ground():
     roles = {"warrior": [200, 20, "Dimension Strike", "Iron Slash", "Defence"],"archer": [150, 30, "Fire Arrow", "Multiple Arrow", "Defence"],"dagger": [120, 40, "One Point Slash", "Back Slash", "Defence"]}
-    playerrole = player()
-    Health = playerrole[0]
-    Attack = playerrole[1]
-    for role, stats in roles.items():
-        print(f"your enemy: {role}:Hp: {stats[0]}Attack: {stats[1]}")
+    
+    playerRole = player()
+    health = playerRole[0]
+    attack = playerRole[1]
+    
+    for enemy, stats in roles.items():
+        
+        print(f"Your enemy: {enemy}:Hp: {stats[0]} attact: {stats[1]}")
+        
         while True:
-            print(f"your HP: {player[0]} \n Your enemy Hp:{stats[0]}")
-            print(f"1.{playerrole[2]}\n2.{playerrole[3]}\n3.{playerrole[4]}")
-            
-            userAttack = int(input("enter number of Attack (1-3): "))
-            if userAttack!= 1 or userAttack != 2 or userAttack !=3:
-                print("inivalid attack number")
+            print(f"Your HP: {playerRole[0]} \nYour enemy Hp: {stats[0]}")
+            print(f"1.{playerRole[2]}\n2.{playerRole[3]}\n3.{playerRole[4]}")
+            userAttack = int(input("Enter number of Attack (1-3): "))
+            if userAttack not in [1, 2, 3]:
+                print("Invalid attack number")
                 continue
             enemyAttack = random.randint(1, 3)
-            if (userAttack == 1 and enemyAttack == 2) or (userAttack == 2 and enemyAttack == 3) or (userAttack == 3 and enemyAttack == 1):
-                stats[0] -= playerrole[1]
-                print(f"you hit the enemy with {playerrole[userAttack+2]} !")
-            elif (userAttack == 1 and enemyAttack == 3) or(userAttack ==3 and enemyAttack== 2) or (userAttack == 2 and enemyAttack ==1):
-                playerrole[0] -= stats[1]
-                print(f"Enemy hits you with {stats[enemyAttack+2]}!")
+            if (userAttack == 1 and enemyAttack == 2) or(userAttack == 2 and enemyAttack == 3) or(userAttack == 3 and enemyAttack == 1):
+                stats[0] -= playerRole[1]
+                print(f"You hit the enemy with {playerRole[userAttack + 1]}!")
+                
+                
+            elif (userAttack == 1 and enemyAttack == 3) or (userAttack == 3 and enemyAttack == 2) or (userAttack == 2 and enemyAttack == 1):
+                
+                playerRole[0] -= stats[1]
+                print(f"Enemy hits you with {stats[enemyAttack + 1]}!")
+            
+            
             else:
-                print("both launch the same attack.No damage")
-
+                print("Both launch the same attack. No damage")
             if stats[0] <= 0:
-                Health += 30
-                playerrole[0]=Health
-                Attack += 10
-                playerrole[1]=Attack
-                print(f"You won!your HP increases to {playerrole[0]} and Attack to {playerrole[1]}")
-                break
-            elif playerrole[0] <= 0:
+                playerRole[0] = health + 30
+                health= playerRole[0]  
+                playerRole[1] = attack + 10
+                attack=playerRole[1]
+                print(f"You won! Your HP increases to {playerRole[0]} and Attack to {playerRole[1]}")
+                break 
+            elif playerRole[0] <= 0:
                 print("You lost!")
                 break
-        if playerrole[0] <= 0:
-            break
+        if playerRole[0] <= 0: 
+            break 
+                
     print("Congratulations! You defeated all enemies.")
 
 battle_ground()
